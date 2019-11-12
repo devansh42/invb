c603bc69c5955f95dcbf844bc46736c16b3fc529
aang <devanshguptamrt@gmail.com>
2019-11-13 00:02:02 +0530

    backend support for kv and job_modifier components

---
+++ routes/app/master/kv.js
@@ -0,0 +1,126 @@
+//This file contains code for kv endpoint 
+const j = require("@hapi/joi");
+const mysql = require("mysql2/promise");
+const env = require("../../../env");
+const err = require("../../../err");
+const express = require("express");
+const router = express.Router();
+
+
+
+router.post("/create", createModifyValidtor, create);
+router.post("/modify", createModifyValidtor, modify);
+router.post("/read", readValidtor, read);
+router.post("/delete", deleteValidator, deleteFn);
+
+const deleteValidator = (req, res) => {
+    const b = req.body;
+    const o = j.object({ id: j.number().required() });
+    if (o.validate(b) == null) {
+        res.json({ error: true, errorMsg: "Invalid  request parameters", code: err.BadRequest });
+    }
+    else next();
+}
+
+const deleteFn = async (req, res) => {
+    const { body } = req;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    const sql = "delete from kv_pair where id= ?";
+    const pstmt = await conn.prepare(sql);
+    const r = await pstmt.execute([body.id]);
+    if(r.rowsAffected==1){
+        res.json({error:false});
+    }else{
+        //couldn't delete anything
+        res.json({error:true,errorMsg:"Invalid KV pair",code:err.BadRequest});
+    }
+
+    if(pstmt==undefined)conn.close(); //connectio closed
+    else pstmt.close().then(()=>conn.close());
+}
+
+
+
+const readValidtor = (req, res) => {
+    const b = req.body;
+    const o = j.object({
+        id: j.number().required()
+    });
+
+    if (o.validate(b) == null) {
+        res.json({ error: true, errorMsg: "Invalid input supplied", code: err.BadRequest });
+    }
+    else next();
+};
+
+const read = async (req, res) => {
+    const { body } = req;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    const sql = "select *from kv_pair where entity=?";
+    const pstmt = await conn.prepare(sql);
+    const [r] = await pstmt.execute([body.id]);
+    res.json({ error: false, result: r });
+    //Closing connections 
+    if (pstmt != undefined) pstmt.close().then(() => conn.close());
+    else conn.close();
+}
+
+const createModifyValidtor = (req, res) => {
+    const b = req.body;
+    const o = j.object({
+        kv_pairs: j.array({
+            id: j.number().required(),
+            kv_key: j.string().required().max(250),
+            kv_value: j.string().required().max(250)
+        }).required()
+    });
+    if (o.validate(b) == null) {
+        res.json({ error: true, errorMsg: "Invalid Input Supplied", code: err.BadRequest });
+    } else next();
+}
+
+const createOrModify = async (req, res, state) => {
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    const { body } = req;
+    let sql = "select kv_key from kv_pair where entity=?";
+    let pstmt = await conn.prepare(sql);
+    let [r] = await pstmt.execute([body.id]);
+    const keys = body.kv_pairs.map(v => kv_key.toLowerCase());
+
+
+    if (state) { //create new
+        for (let i = 0; i < r.length; i++) {
+            if (keys.indexOf(r[i].kv_key.toLowerCase()) != -1) {
+                //Duplicate kv pair
+                res.json({ error: true, errorMsg: "Duplicate KV Pair", code: err.Duplicate });
+                pstmt.close().then(() => conn.close());//Closing db connections
+                return;
+            }
+        }
+
+        sql = "INSERT INTO inv.kv_pair(kv_key,kv_value,entity)VALUES(?,?,?)";
+        pstmt = await conn.prepare(sql);
+        const ar = body.kv_pairs.map(v => pstmt.execute([v.kv_key, v.kv_value, v.id]));
+        await Promise.all(ar);
+    } else {
+        sql = "update kv_pair set kv_value = ? where kv_key = ? and entity = ?";
+        pstmt = await conn.prepare(sql);
+        const ar = body.kv_pairs.map(v => pstmt.execute([v.kv_value, v.kv_key, v.id]));
+        await Promise.all(ar);
+    }
+    res.json({ error: false });
+    if (pstmt == undefined) conn.close();
+    else pstmt.close().then(() => { conn.close() });
+
+}
+
+
+let create = (req, res) => {
+    createOrModify(req, res, true);
+};
+let modify = (req, res) => {
+    createOrModify(req, res, false);
+}
+
+
+module.exports = router;
---
+++ routes/app/production/job_modifier.js
@@ -0,0 +1,193 @@
+//This file contains endpoint definition for job card modifier
+//Which actually controls the job modifier
+
+
+const j = require("@hapi/joi");
+const mysql = require("mysql2/promise");
+const env = require("../../../env");
+const err = require("../../../err");
+const express = require("express");
+const SerialSeq = require("../../../entity/serialSeq");
+const router = express.Router();
+
+
+//router.post("/create", createModifyValidtor, create);
+//router.post("/modify", createModifyValidtor, modify);
+router.post("/read", readValidtor, read);
+router.post("/delete", deleteValidtor, deleteFn);
+router.post("/add", addValidator, add);
+router.post("/action", actionValidator, action);
+
+const actionValidator = (req, res, next) => {
+    const { body } = req;
+    const o = j.object({
+        job_card: j.number().required(),
+        action: j.string().allow(["start", "finish"]),
+        time: j.number().required()
+    });
+    if (o.validate(body) == null) {
+        res.json({ error: true, errorMsg: "invalid Request Parameters", code: err.BadRequest });
+
+    } else next()
+}
+
+const action = async (req, res) => {
+    const { body } = req;
+    const b = body;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    let sql = "select st_time,fi_time,state from job_card where id= ?";
+    let pstmt = await conn.prepare(sql);
+    let [r] = await pstmt.execute([b.job_card]);
+    let error = { error: false };
+    if (r.length > 0) {
+        const { st_time, fi_time, state } = r[0];
+        let nextState = 0;
+        switch (action) {
+            case "start":
+                if (state == 1) {
+                    sql = "update job_card set st_time = ? and state = ? where id = ? limit 1";
+                    nextState = 2;
+                } else error = {
+                    error: true,
+                    errorMsg: "Invalid action, operation is started previously",
+                    code: err.BadRequest
+                }
+
+                break;
+            case "finish":
+                if (state > 2) {
+                    sql = "update job_card set fi_time = ? and state = ? where id = ? limit 1";
+                    nextState = 4;
+                } else error = {
+                    error: true,
+                    errorMsg: "Invalid action, operation cannot be finished",
+                    code: err.BadRequest
+                }
+                break;
+            default:
+                error = {
+                    error: true,
+                    errorMsg: "Invalid action",
+                    code: err.BadRequest
+                }
+                break;
+        }
+        if (error.error) {
+            //Error
+        } else {
+            pstmt = await conn.prepare(sql);
+            await pstmt.execute([body.time, nextState, body.job_card]);
+            error.nextState = nextState; //new state of operation
+            if (action == "finish") {
+                //checking state of workorder
+            }
+        }
+        res.json(error);
+
+    } else {
+        res.json({ error: true, errorMsg: "Invalid Job Card Id", code: err.BadRequest });
+    }
+    pstmt.close().then(() => { conn.close() });
+
+}
+
+
+const addValidator = (req, res, next) => {
+    const { body } = req;
+    const o = j.object({
+        job_card: j.number(),
+        logs: j.array({
+            worker: j.number(),
+            st_time: j.number(),
+            en_time: j.number(),
+            description: j.string()
+        })
+    });
+    if (o.validate(body) == null) {
+        res.json({ error: true, errorMsg: "Invalid Request Parameters", code: err.BadRequest });
+    } else next();
+}
+
+const add = async (req, res) => {
+    const { body } = req;
+    const b = body;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    const sql = "INSERT INTO inv.job_logs(job_card,worker,st_time,en_time,description)VALUES(?,?,?,?,?)";
+
+    const pstmt = await conn.prepare(sql);
+    const ar = body.logs.map(v => pstmt.execute([body.job_card, v.worker, v.st_time, v.en_time, v.description]));
+    await Promise.all(ar);
+    pstmt.close().then(() => { conn.close() });
+    res.json({ error: false });
+
+}
+
+const deleteValidtor = (req, res, next) => {
+    const { body } = req;
+    const o = j.object({
+        id: j.array().items(...j.number())
+    });
+
+    if (o.validate(body) == null) {
+        res.json({ error: true, errorMsg: "Invalid Request Parameters", code: err.BadRequest });
+    }
+    else next();
+}
+
+const deleteFn = async (req, res) => {
+    const { body } = req;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    const sql = "delete from job_logs where id = ? limit 1";
+    const pstmt = await conn.prepare(sql);
+    const ar = body.id.map(v => pstmt.execute([v]));
+    await Promise.all(ar);
+    pstmt.close().then(() => { conn.close() });
+    res.json({ error: false });
+}
+
+const readValidtor = (req, res, next) => {
+    const b = req.body;
+    const o = j.object({ id: j.number().required() });
+    if (o.validate(b) == null) {
+        res.json({ error: false, errorMsg: "Invalid request parameters", code: err.BadRequest });
+    } else next();
+}
+
+const read = async (req, res) => {
+    const { body } = req;
+    const conn = await mysql.createConnection(env.MYSQL_Props);
+    let sql, pstmt;
+    sql = "select sn.sn,sn.ser_seq,j.entityId,j.id,j.workorder,j.operation,j.qty,j.post_time,j.st_time,j.fi_time,j.state,s.name as state_name,o.name from job_card as j join operation as o on o.id=j.operation join process_states as s on s.id=j.state join serial_nos as sn on sn.id=j.entityId where j.id=? limit 1";
+    pstmt = await conn.prepare(sql);
+    let [r] = await pstmt.execute([body.id]);
+    let result;
+    if (r.length > 0) {
+        result = { job_card: r[0] };
+        if (r[0].entityId != null) {
+            //Checking if item has serial no
+            sql = "select *from serial_no_seq where id=? limit 1";
+            pstmt = await conn.prepare(sql);
+            [r] = await pstmt.execute([result.job_card.ser_seq]);
+            const sn = new SerialSeq();
+            sn.populateFromObject(r[0]);
+            result.job_card.serial_no = sn.getSerialNo(result.job_card.sn);
+        }
+        sql = "select j.id,j.job_card,j.worker,j.st_time,j.en_time,j.description,a.name as worker_name from job_logs as j join  account as a on a.id = j.worker where j.job_card=?";
+        pstmt = await conn.prepare(sql);
+        [r] = pstmt.execute([body.id]);
+        result.job_logs = r; //saving logs
+        //Now, we will
+
+
+
+        res.json({ error: false, result });
+    } else {
+        //no result found invalid id
+        res.json({ error: true, errorMsg: "Invalid Job Card Id", code: err.BadRequest });
+    }
+    if (pstmt == undefined) conn.close();
+    else pstmt.close().then(() => { conn.close() });
+
+}
+
+module.exports = router;
