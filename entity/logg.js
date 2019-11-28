969492fa50e775c84c59e609da03d854592702cc
aang <devanshguptamrt@gmail.com>
2019-11-29 01:16:21 +0530

    logging utility for backend

---
+++ entity/logg.js
@@ -0,0 +1,7 @@
+//This file contains code for simple logging solution used in this project
+/**
+ * Logger for logging, by default uses console
+ */
+
+const log = console.log;
+module.exports = { log };
